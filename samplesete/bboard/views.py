289026485b8f5f8718312017739271f90f086cd4
Эдуард Кусков <shadowfish@yandex.ru>
2024-06-22 15:04:50 +0300

    page 72, Уже по django 4

--- samplesete/bboard/views.py
+++ samplesete/bboard/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 
 from bboard.models import Bb, Rubric
 from django.template import loader
+from django.views.generic import CreateView
+
+from bboard.forms import BbForm
 
 
 # Create your views here.
@@ -27,3 +30,14 @@ def by_rubric(request, rubric_id):
     current_rubric = Rubric.objects.get(pk=rubric_id)
     context = {'bbs': bbs, 'rubrics': rubrics, 'current_rubric': current_rubric}
     return render(request, 'bboard/by_rubric.html', context)
+
+
+class BbCreateView(CreateView):
+    template_name = 'bboard/bb_create.html'
+    form_class = BbForm
+    success_url = '/bboard/'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rubrics'] = Rubric.objects.all()
+        return context
