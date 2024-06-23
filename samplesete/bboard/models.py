058598f787a7ca6f23283eb005c2d44f58fa134b
Эдуард Кусков <shadowfish@yandex.ru>
2024-06-23 23:24:20 +0300

    pg 110

--- samplesete/bboard/models.py
+++ samplesete/bboard/models.py
@@ -4,6 +4,41 @@ NULLABLE = {'null': True, 'blank': True}
 
 
 class Bb(models.Model):
+    BUY = 'b'
+    SELL = 's'
+    EXCHANGE = 'c'
+
+    KINDS = (
+        (None, 'Выберите тип публикуемого объявления'),
+        ('Купля-продажа', (
+            (BUY, 'Куплю'),
+            (SELL, 'Продам'),
+        )),
+        ('Обмен', (
+            (EXCHANGE, 'Обменяю'),
+        ))
+    )
+    """
+    class Kinds(models.TextChoices):
+        BUY = 'b', 'Куплю'
+        SELL = 's', 'Продам'
+        EXCHANGE = 'c', 'Обменяю'
+        RENT = 'r'
+        __empty__ = 'Выберите тип публикуемого объявления'
+        
+    kind = models.CharField(max_length=1, choices=Kinds.choices, default=Kinds.SELL)
+    
+    
+    class Kinds(models.IntegerChoices):
+        BUY = 1, 'Куплю'
+        SELL = 2, 'Продам'
+        EXCHANGE = 3, 'Обменяю'
+        RENT = 4
+        
+    kind = models.SmallIntegerField(choices=Kinds.choices, default=Kinds.SELL)
+    """
+    kind = models.CharField(max_length=1, choices=KINDS, blank=True)
+
     rubric = models.ForeignKey('Rubric', null=True, on_delete=models.PROTECT, verbose_name='Рубрика')
     title = models.CharField(max_length=50, verbose_name='Товар')
     content = models.TextField(**NULLABLE, verbose_name='Описание')
